9adf9a2becc455dd41e8e55f74924fbcc62c8614
Isaac Bickel <96277815+Ch0pstix@users.noreply.github.com>
2023-02-20 09:35:37 -0500

    Refactor + Rename GlobalUsing.cs
    Rename GlobalUsing.cs

--- src/EditorSmoothScroll/Properties/GlobalUsings.cs
+++ src/EditorSmoothScroll/Properties/GlobalUsings.cs
@@ -1,20 +1,22 @@
 ﻿global using System;
 global using System.ComponentModel;
 global using System.ComponentModel.Composition;
+global using System.Linq;
 global using System.Reflection;
 global using System.Runtime.InteropServices;
 global using System.Threading;
 global using System.Threading.Tasks;
 global using System.Windows.Input;
+global using DisplayName = System.ComponentModel.DisplayNameAttribute;
 
 global using Community.VisualStudio.Toolkit;
-
-global using Microsoft.VisualStudio;
-global using Microsoft.VisualStudio.Shell;
-global using Microsoft.VisualStudio.Text.Editor;
-global using Microsoft.VisualStudio.Utilities;
 
 global using EditorSmoothScroll;
 global using EditorSmoothScroll.Options;
 global using EditorSmoothScroll.Common;
 global using Package = EditorSmoothScroll.EditorSmoothScrollPackage;
+
+global using Microsoft.VisualStudio;
+global using Microsoft.VisualStudio.Shell;
+global using Microsoft.VisualStudio.Text.Editor;
+global using Microsoft.VisualStudio.Utilities;
