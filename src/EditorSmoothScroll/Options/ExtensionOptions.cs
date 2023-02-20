22d43267bc8e37fb2a79670b7136d2df588d6098
Isaac Bickel <96277815+Ch0pstix@users.noreply.github.com>
2023-02-20 13:20:05 -0500

    Update ExtensionOptions.cs

--- src/EditorSmoothScroll/Options/ExtensionOptions.cs
+++ src/EditorSmoothScroll/Options/ExtensionOptions.cs
@@ -1,7 +1,5 @@
 ﻿namespace EditorSmoothScroll.Options;
 
-using DisplayName = System.ComponentModel.DisplayNameAttribute;
-
 internal partial class OptionsProvider
 {
 	[ComVisible(true)]
@@ -11,19 +9,14 @@ internal partial class OptionsProvider
 public class ExtensionOptions : BaseOptionModel<ExtensionOptions>
 {
 	[Category("General")]
-	[DisplayName("Enable editor smooth scroll")]
+	[DisplayName("Enabled")]
 	[Description("Sets whether or not the extension is enabled.")]
 	[DefaultValue(true)]
 	public bool IsExtensionEnabled { get; set; } = true;
 
 	[Category("General")]
-	[DisplayName("Scroll horizontally with shift")]
-	[Description("Sets whether or not the shift key should cause the editor to scroll horizontally.")]
-	public bool UseShiftForHorizontalScrolling { get; set; } = true;
-
-	[Category("General")]
-	[DisplayName("Easing function")]
+	[DisplayName("Smoothing function")]
 	[Description("Sets the easing function that the smooth scrolling engine should use.")]
-	[DefaultValue(EasingFunction.Quadratic)]
-	public EasingFunction EasingFunction { get; set; } = EasingFunction.Quadratic;
+	[DefaultValue(EasingFunction.Quintic)]
+	public EasingFunction EasingFunction { get; set; } = EasingFunction.Quintic;
 }
